a4ba4e14a354f4777d10fb28a6292a6b410e6456
Phong Nguyen <phong204n@gmail.com>
2026-09-16 15:06:32 -0700

    Pseudo coding.

--- app/myenv.py
+++ app/myenv.py
@@ -6,6 +6,7 @@ readme.
 Delete this docstring and describe your own world instead.
 """
 
+import random
 import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
@@ -19,15 +20,26 @@ class MyEnv(gym.Env):
 
     def __init__(self, render_mode: str | None = None):
         # TODO: describe your world here -- the map, the pieces, the constants.
+        self.CONST_DIRECTIONS = 4
+        self.CONST_WORLD_X = 10
+        self.CONST_WORLD_Y = self.CONST_WORLD_X
+
+        self.world_state = [[]]
+        self.reset_world()
 
         # TODO: set the two spaces. Both must be Discrete.
 
-        self.observation_space = # TODO
-        self.action_space = # TODO
+        self.observation_space = spaces.Box(
+            low=0,high=5,shape=(self.CONST_WORLD_X,self.CONST_WORLD_Y),dtype=np.uint8
+        )
+        self.action_space = spaces.Discrete(self.CONST_DIRECTIONS, start=2)
 
         if render_mode is not None and render_mode not in self.metadata["render_modes"]:
             raise ValueError(f"unsupported render_mode: {render_mode}")
         self.render_mode = render_mode
+
+    def _get_obs(self):
+        return self.world_state
 
     def reset(self, seed: int | None = None, options: dict | None = None):
         # This line seeds self.np_random. Without it, seeding does not work and
@@ -35,6 +47,8 @@ class MyEnv(gym.Env):
         super().reset(seed=seed)
 
         # TODO: put the world back to its starting state.
+        self.reset_world()
+        self.random_spawn()
 
         return self._get_obs(), self._get_info()
 
@@ -45,6 +59,37 @@ class MyEnv(gym.Env):
         # agent died, game over. Leave truncated as False and let the TimeLimit
         # wrapper from register() handle running out of time. The agent treats
         # the two differently, and so should you.
+
+        ##  TODO:: PSEUDO-CODE
+        ##  for x in world_length
+        ##      for y in world_height
+        ##          head_exists = false
+        ##          if cell[x][y] > 1, is snake
+        ##              facing init
+        ##              match cell[x][y]
+        ##                  case 2 is up
+        ##                      if y is 0, terminate, subtract point
+        ##                      facing = cell[x][y-1]
+        ##                  case 3 is right
+        ##                      if x = world_length, terminate, subtract point
+        ##                      facing = cell[x+1][y]
+        ##                  case 4 is down
+        ##                      if y = world_height, terminate, subtract point
+        ##                      facing = cell[x][y+1]
+        ##                  case 5 is left
+        ##                      if x = 0, terminate, subtract point
+        ##                      facing = cell[x-1][y]
+        ##              
+        ##              if facing > 1, is snake, is body
+        ##              elif facing < 2, is good
+        ##                  if facing is 1, is fruit
+        ##                      add point
+        ##                      todo increase
+        ##                  
+        ##                  
+        ##      if head_exists is false, is dead
+        ##          terminate, subtract point
+        
 
         raise NotImplementedError
 
@@ -57,6 +102,22 @@ class MyEnv(gym.Env):
 
     def close(self):
         pass
+
+    def reset_world(self):
+        self.world_state = [
+            [0 for x in range(0, self.CONST_WORLD_X)]
+            for y in range(0, self.CONST_WORLD_Y)
+        ]
+
+    def random_spawn(self):
+        start_x = random.randint(1,self.CONST_WORLD_X-2)
+        start_y = random.randint(1, self.CONST_WORLD_Y-2)
+
+        ## TODO:: Revisit.  Directions start at 2.  2==UP. 3==RIGHT. 4==DOWN.  5==LEFT.
+        ##          -  This is because 0==BLANK.  1==FRUIT.
+        start_dir = random.randint(2, 2+self.CONST_DIRECTIONS-1)
+
+        self.world_state[start_x][start_y] = start_dir
 
 
 # TODO: name your environment. The id must start with "cs272/" and end with a
---
+++ app/snakegame.py
@@ -0,0 +1,4 @@
+class snakegame:
+    def __init__(self):
+        pass
+    
